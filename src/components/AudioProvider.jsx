a738ca28c6cb7409d52fbac95711c6a5630611dd
MayanKiz <140157357+MayanKiz@users.noreply.github.com>
2025-12-24 11:06:24 +0530

    Update AudioProvider.jsx

--- src/components/AudioProvider.jsx
+++ src/components/AudioProvider.jsx
@@ -5,19 +5,21 @@ import { createContext, useContext, useRef } from "react"
 const AudioContext = createContext(null)
 
 export function AudioProvider({ children }) {
-    const audioRef = useRef(null)
+  const audioRef = useRef(null)
 
-    return (
-        <AudioContext.Provider value={audioRef}>
-            <audio
-                ref={audioRef}
-                src="/gifs/Aaj-Se-Teri-Slowed-Reverb-Arijit-Singh-Padman-Akshay-Kumar-R.m4a"
-                preload="auto"
-                loop
-            />
-            {children}
-        </AudioContext.Provider>
-    )
+  return (
+    <AudioContext.Provider value={audioRef}>
+      <audio
+        ref={audioRef}
+        src="/gifs/Aaj-Se-Teri-Slowed-Reverb-Arijit-Singh-Padman-Akshay-Kumar-R.m4a"
+        preload="auto"
+        loop
+      />
+      {children}
+    </AudioContext.Provider>
+  )
 }
 
-export const useGlobalAudio = () => useContext(AudioContext)
+export function useGlobalAudio() {
+  return useContext(AudioContext)
+}
