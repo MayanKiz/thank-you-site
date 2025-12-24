f5dfef0413e693223638f5ea29adec777eb5c7c4
MayanKiz <140157357+MayanKiz@users.noreply.github.com>
2025-12-24 11:06:00 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,19 @@
 import "./globals.css";
+import { AudioProvider } from "./AudioProvider";
 
 export const metadata = {
   title: "I Have Something to Say...",
-  description: "This isn’t just a website. It’s something truly special, built with love — just for you. Open it and feel it unfold.",
+  description:
+    "This isn’t just a website. It’s something truly special, built with love — just for you.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`antialiased`}
-      >
-        {children}
+      <body className="antialiased">
+        <AudioProvider>
+          {children}
+        </AudioProvider>
       </body>
     </html>
   );
