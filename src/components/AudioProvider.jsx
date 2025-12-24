195cb74374c9c2ebb1c4e4c7d2e1faf0df4508cd
MayanKiz <140157357+MayanKiz@users.noreply.github.com>
2025-12-24 11:04:30 +0530

    Create AudioProvider.jsx

---
+++ src/components/AudioProvider.jsx
@@ -0,0 +1,23 @@
+"use client"
+
+import { createContext, useContext, useRef } from "react"
+
+const AudioContext = createContext(null)
+
+export function AudioProvider({ children }) {
+    const audioRef = useRef(null)
+
+    return (
+        <AudioContext.Provider value={audioRef}>
+            <audio
+                ref={audioRef}
+                src="/gifs/Aaj-Se-Teri-Slowed-Reverb-Arijit-Singh-Padman-Akshay-Kumar-R.m4a"
+                preload="auto"
+                loop
+            />
+            {children}
+        </AudioContext.Provider>
+    )
+}
+
+export const useGlobalAudio = () => useContext(AudioContext)
